290b22bd19f913f41713f09f9554229e5e65feb0
RCh <rch@null>
2020-08-04 12:38:26 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/file_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_search.dir/main.cpp.o"
+  "file_search"
+  "file_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
